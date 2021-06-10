8628a01839279a76f06abba01132adb921af0c22
pavel <pavel.beranek@centrum.cz>
2021-06-10 21:37:35 +0200

    remove contextual menu, add offcanvas menu

--- static/js/printPage.js
+++ static/js/printPage.js
@@ -1,7 +1,7 @@
 function dimPage(){
     let height = document.body.scrollHeight;
     let overlay = document.createElement('div');
-    overlay.style.cssText = 'position:absolute;width:100%;height:100%;opacity:0.75;z-index:1000;background:#000;top:0;left:0; display:flex; align-items:center; justify-content:center;';
+    overlay.style.cssText = 'position:absolute;width:100%;height:100%;opacity:0.75;z-index:2000;background:#000;top:0;left:0; display:flex; align-items:center; justify-content:center;';
     overlay.innerHTML = '<img src="/static/ico/loader.svg" alt="spinner" style="z-index:5000;">'
     document.body.appendChild(overlay);
 
